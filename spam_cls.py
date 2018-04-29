8ae16dd8a2a23fab2403bd819dff8e2c3b49c3b5
Lior <lior@revuze.it>
2018-04-29 20:18:47 +0300

    Minor changes.
    Signed-off-by: Lior <lior@revuze.it>

--- spam_cls.py
+++ spam_cls.py
@@ -1,15 +1,14 @@
-from collections import Counter
-
 import pandas as pd
 from keras.preprocessing.text import text_to_word_sequence
 from nltk import SnowballStemmer, re, downloader
 from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.svm import LinearSVC
+import numpy as np
 import matplotlib.pyplot as plt
 import statistics
 import os
@@ -55,14 +54,14 @@ def trivial_tokenizer(text):
     return text
 
 
-def is_long_number(text, threshold=5, flag_res=False):
+def is_long_number(text, threshold=1, flag_res=False):
     numbers_lens = re.findall('\\d+', text)
     if numbers_lens and len(max(numbers_lens, key=len)) >= threshold:
         if flag_res:
-            return True
+            return len(max(numbers_lens, key=len))
         return text + ' _longnumber_'
     if flag_res:
-        return False
+        return 0
     return text
 
 
@@ -79,43 +78,81 @@ x_train_ = train[fields[1]].fillna("fillna").values.tolist()  # Make sure no cel
 y_train = train[fields[0]].values
 
 # Showing some data characteristics
-class_convert = {1: ['Spam'], 0: ['Not Spam']}
-number_hist = Counter()
-emph_counter = {'emph': [], 'non emph': []}
+positive_class = 'not_spam'
+negative_class = 'spam'
+class_convert = {1: [negative_class], 0: [positive_class]}
+
+spam_number_counter = []
+non_spam_number_counter = []
+
 spam_emph, non_spam_emph = [], []
 spam_urls, non_spam_urls = [], []
+
+non_spam_docs_count = 0
+spam_docs_count = 0
+
+spam_money_count = 0
+non_spam_money_count = 0
+
 for x_, y_ in zip(x_train_, y_train):
-    if is_long_number(x_, flag_res=True):
-        number_hist.update(class_convert[int(y_)])
     emph_count = convert_emphesize(x_, return_count=True)
     if emph_count:
-        # ratio_ = round(len(x_.split(' ')) / emph_count)
         ratio_ = emph_count
         if (y_ == [0]).all():
             non_spam_emph.append(ratio_)
         else:
             spam_emph.append(ratio_)
-    # if strip_url(x_, return_count=True) > 0:
+
     if (y_ == [0]).all():
+        if '£' in x_:
+            non_spam_money_count += 1
+        non_spam_docs_count += 1
+        non_spam_number_counter.append(is_long_number(x_, flag_res=True))
         non_spam_urls.append(strip_url(x_, return_count=True))
     else:
+        if '£' in x_:
+            spam_money_count += 1
+        spam_docs_count += 1
+        spam_number_counter.append(is_long_number(x_, flag_res=True))
         spam_urls.append(strip_url(x_, return_count=True))
 
-print('Number of occurrences of long numbers in non-spam sentences: %s' % number_hist[0])
-print('Number of occurrences of long numbers in spam sentences: %s' % number_hist[1])
+print('Number of occurrences of long (more than 4 following digits) numbers in non-spam sentences: %s/%s'
+      % (sum(i > 4 for i in non_spam_number_counter), non_spam_docs_count))
+print('Number of occurrences of long (more than 4 following digits) numbers in spam sentences: %s/%s\n' %
+      (sum(i > 4 for i in spam_number_counter), spam_docs_count))
 
-print('Trying to prove that spam messages contain more emphasized words than non-spam messages')
+print('Number of occurrences of long (more than 3 following digits) numbers in non-spam sentences: %s/%s'
+      % (sum(i > 3 for i in non_spam_number_counter), non_spam_docs_count))
+print('Number of occurrences of long (more than 3 following digits) numbers in spam sentences: %s/%s\n' %
+      (sum(i > 3 for i in spam_number_counter), spam_docs_count))
+
+print('Number of occurrences of long (more than 2 following digits) numbers in non-spam sentences: %s/%s'
+      % (sum(i > 2 for i in non_spam_number_counter), non_spam_docs_count))
+print('Number of occurrences of long (more than 2 following digits) numbers in spam sentences: %s/%s\n' %
+      (sum(i > 2 for i in spam_number_counter), spam_docs_count))
+
+print('Based on the fact that in most of the spam messages there are long numbers (we''l stick to emphasizing number '
+      'that are longer than 4 digits) we will add a special terms that will contain this information as part of the '
+      'text')
+
+print('Money sign (Pound) occurred in %s/%s spam docs' % (spam_money_count, spam_docs_count))
+print('Money sign (Pound) occurred in %s/%s non-spam docs' % (non_spam_money_count, non_spam_docs_count))
+
+print('Based on the fact that in most spam messages we see a Pound sign, we''ll keep this special '
+      'sign for better classification results\n')
+
+print('Trying to prove that spam messages contains more emphasized words than non-spam messages')
 fig1 = plt.figure(1)
 plt.hist(spam_emph)
 plt.title('Spam emphasized words histogram - avg=%.3f, std=%.3f' % (statistics.mean(spam_emph),
-                                                                          statistics.stdev(spam_emph)))
+                                                                    statistics.stdev(spam_emph)))
 plt.xlabel('Ratio')
 plt.ylabel('Count')
 fig1.show()
 
 fig2 = plt.figure(2)
 plt.title('Not-spam emphasized words histogram - avg=%.3f, std=%.3f' % (statistics.mean(non_spam_emph),
-                                                                              statistics.stdev(non_spam_emph)))
+                                                                        statistics.stdev(non_spam_emph)))
 plt.hist(non_spam_emph)
 plt.xlabel('Ratio')
 plt.ylabel('Count')
@@ -128,30 +165,8 @@ plt.legend(loc='upper right')
 plt.title('Emphasized words count - Spam VS not-spam')
 fig3.show()
 
-fig4 = plt.figure(3)
-plt.hist(spam_urls, label='Spam')
-plt.hist(non_spam_urls, label='Not spam')
-plt.legend(loc='upper right')
-plt.title('Number of URLs in text - Spam VS not-spam')
-fig4.show()
-
-# Showing that in spam messages there's a dominant amount of URLs
-# class_convert = {1: ['Spam'], 0: ['Not Spam']}
-# number_hist = Counter()
-# emph_counter = {'url exist': [], 'no url': []}
-# spam_urls, non_spam_urls = [], []
-# for x_, y_ in zip(x_train_, y_train):
-#     if strip_url(x_, return_count=True):
-#         number_hist.update(class_convert[int(y_)])
-#     emph_count = convert_emphesize(x_, return_count=True)
-#     if emph_count:
-#         # ratio_ = round(len(x_.split(' ')) / emph_count)
-#         ratio_ = emph_count
-#         if (y_ == [0]).all():
-#             non_spam_emph.append(ratio_)
-#         else:
-#             spam_emph.append(ratio_)
-
+print('\nURLs exists in %s/%s of the spam docs' % (sum(i > 0 for i in spam_urls), spam_docs_count))
+print('URLs exists in %s/%s of the non-spam docs' % (sum(i > 0 for i in non_spam_urls), non_spam_docs_count))
 
 # Pre Processing data
 lengths = 0
@@ -170,7 +185,8 @@ for idx, doc in enumerate(x_train_):
     lengths += len(x_train_[idx])
 
 max_len = round(lengths / idx)
-print('Average length: %s' % max_len)
+# Maybe I should get the average length of a spam document VS a non spam document
+print('Average document length: %s' % max_len)
 
 x_train, x_test, y_train, y_test = train_test_split(x_train_, y_train,
                                                     test_size=test_size)
