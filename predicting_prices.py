f12d33cac34381c85f222264296cf7d92f97ad5d
Lior <lior@revuze.it>
2018-05-03 11:07:58 +0300

    Final version after adding logger and printing times as well.
    Signed-off-by: Lior <lior@revuze.it>

--- predicting_prices.py
+++ predicting_prices.py
@@ -1,36 +1,89 @@
 import os
+import time
 import pandas as pd
 import logging
-from statsmodels.tsa.ar_model import AR
 import math
-import matplotlib.pyplot as plt
+
+from statsmodels.tsa.ar_model import AR
 
 
 DATA_PATH = os.path.join(os.getcwd(), 'data', 'productsPrices.csv')
 
 
 # This is a time series regression problem where I need to forecast the next day price
-# TODO: Great tutorial for such problems
-# TODO: Add timing to each part
-# TODO: https://datascienceplus.com/linear-regression-in-python-predict-the-bay-areas-home-prices/
-# TODO: https://towardsdatascience.com/create-a-model-to-predict-house-prices-using-python-d34fe8fad88f
+
+def _logger(file_name='predicting_prices.log'):
+    level = logging.INFO
+    format = '%(asctime)s - %(process)s-%(thread)-6.6s - %(filename)-15.15s %(funcName)-15.15s ' \
+             '%(lineno)-4.4s - %(levelname)-6.6s - %(message)s'
+    handlers = [logging.FileHandler(file_name), logging.StreamHandler()]
+    logging.basicConfig(level=level, format=format, handlers=handlers)
+    return logging
+
+
+class Timer(object):
+    """
+    This class returns timer values.
+    """
+    def __init__(self, p_hours=True, p_minutes=True, p_seconds=True):
+        self.end = None
+        self.start = None
+        self.start_stop_timer()
+        self.p_hours = p_hours
+        self.p_minutes = p_minutes
+        self.p_seconds = p_seconds
+
+    def print_timer(self):
+        self.start_stop_timer(start=False)
+        hours, rem = divmod(self.end - self.start, 3600)
+        minutes, seconds = divmod(rem, 60)
+        if self.p_seconds:
+            if self.p_minutes:
+                if self.p_hours:
+                    return "%d:%02d:%02d (hh:mm:ss)" % (hours, minutes, seconds)
+                else:
+                    return "%02d:%02d (mm:ss)" % (minutes, seconds)
+            else:
+                return "%02d (ss)" % seconds
+
+        if self.p_minutes:
+            if self.p_hours:
+                return "%d:%02d (hh:mm)" % (hours, minutes)
+            else:
+                return "%02d (mm)" % minutes
+
+        if self.p_hours:
+            return "%2d (hh)" % hours
+
+    def start_stop_timer(self, start=True):
+        if start:
+            self.start = time.time()
+        else:
+            self.end = time.time()
 
 
 class PredictPrices(object):
-    def __init__(self, test_size=.2, data_path=None, inspect_data=False, visualize_data=False, n_estimators=10,
-                 number_of_test_values=1):
-        self.logger = logging.getLogger('predict_prices_logger1')
-        print('Prices prediction process starts')
+    """
+    This class trains on a time series data and forecast the next day value
+    """
+    def __init__(self, data_path=None, inspect_data=False, n_estimators=10, number_of_test_values=1):
+        """
+        :param data_path:string The location of the data file (loads CSV files only)
+        :param inspect_data:bool Go over all the input values and make sure everything comes as expected
+        :param n_estimators:int The amount of estimators to use
+        :param number_of_test_values:int The amount of given test values
+        """
+        self.logger.info('Prices prediction process begins')
+        self.logger = _logger()
         self.data_path = data_path
-        self.test_size = test_size
         self.number_of_test_values = number_of_test_values
         self.inspect_data = inspect_data
         self.n_estimators = n_estimators
-        self.visualize_data_ = visualize_data  # Create histogram ()
         self.validate_input()
         self.products = []
         self.model = None
         self.model_fit = None
+        self.t = Timer()
 
     def train(self, x_train, x_test):
         self.model = AR(x_train)
@@ -46,7 +99,6 @@ class PredictPrices(object):
                     else:
                         self.model_fit = self.model.fit(maxlag=i, disp=False, trend=trend)
                     y_predicted = self.predict(history, vanilla_predictor=vanilla)
-                    # y_predicted = self.predict(history)
                     temp_diff = abs(y_predicted - x_test)
 
                     if temp_diff < min_diff:
@@ -64,17 +116,18 @@ class PredictPrices(object):
         coef = self.model_fit.params
         if vanilla_predictor:
             return self.model.predict(params=coef)[0]
-        yhat = coef[0]
 
+        yhat = coef[0]
         for i in range(1, len(coef)):
             yhat += coef[i] * history[-i]
         return yhat
 
     def import_and_arrange_data(self):
         train = pd.read_csv(self.data_path)
-        train.fillna(train.mean())  # Empty cells will be replaced with the average of the product prices
+        train.fillna(
+            train.mean())  # Empty cells (if there are any) will be replaced with the average of the product prices
         if self.inspect_data:
-            print(train.describe())
+            self.logger.info(train.describe())
         self.products = train.columns.tolist()
 
         values_per_product = {}
@@ -83,41 +136,43 @@ class PredictPrices(object):
         return values_per_product
 
     def import_train_and_predict(self):
+        t = Timer()
         x_test_predicted = []
         x_test = []
         train_per_product = self.import_and_arrange_data()
+
         for product, values in train_per_product.items():
+            if self.number_of_test_values >= len(values):
+                raise Exception('The number of test values must be smaller than the amount of train set values')
+
             x_train = values[:-self.number_of_test_values]
             x_test.append(values[-self.number_of_test_values])
             self.model_fit, history, vanilla_predictor = self.train(x_train, values[-self.number_of_test_values])
             x_test_predicted.append(self.predict(history, vanilla_predictor))
+
         for x_test_, x_predicted in zip(x_test, x_test_predicted):
-            print('Predicted value: %.3f, real value: %s' % (x_predicted, x_test_))
-            print('Abs diff between prediction and true value: %.3f\n' % (abs(x_test_ - x_predicted)))
-        # print('Test MSE: %.3f' % predictions_error)
+            self.logger.info('Predicted value: %.3f, real value: %s' % (x_predicted, x_test_))
+            self.logger.info('Abs diff between predicted and true value: %.3f\n' % (abs(x_test_ - x_predicted)))
 
-        # print('Predicted value - %s, real value - %s' % (x_test_predicted, x_test))
-        # error = mean_squared_error([x_test], [x_test_predicted])
-        # print('Test MSE: %.3f' % error)
-
-    def visualize_data(self):
-        pass
+        self.logger.info('Price prediction took %s' % t.print_timer())
 
     def validate_input(self):
+        self.logger.info('Validating input')
         if self.data_path is None:
             raise Exception('You must provide the location of the data')
-        if not isinstance(self.test_size, float):
-            raise TypeError('Test size must be given as a float')
-        if self.test_size < 0 or self.test_size > 1.:
-            raise ValueError('Test size must be between 0.0 - 1.0')
+        if self.data_path[-3:] != 'csv':
+            raise TypeError('This code support CSV files only')
+        if self.number_of_test_values <= 0:
+            raise ValueError('The number of test values must be greater than 0')
+        if not isinstance(self.number_of_test_values, int):
+            raise TypeError('The number of test values must be integer')
         if not isinstance(self.inspect_data, bool):
             raise TypeError('Inspect data must be Boolean')
-        if not isinstance(self.visualize_data_, bool):
-            raise TypeError('Visualize data must be Boolean')
         if not isinstance(self.n_estimators, int):
             raise TypeError('The number of estimators must an integer')
+        self.logger.info('Validation complete')
 
 
 if __name__ == '__main__':
-    pp = PredictPrices(test_size=.2, data_path=DATA_PATH)
+    pp = PredictPrices(data_path=DATA_PATH)
     pp.import_train_and_predict()
